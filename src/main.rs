895ff6d9915496c061303ee25b6723664bdb85dd
hamdymohamedak <mohameddhamdy407@gmail.com>
2024-04-03 16:28:41 +0200

    youTube downloader with rust

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use rustube;
+
+#[tokio::main]
+async fn main() {
+    println!("Enter your name:");
+
+    let mut name = String::new();
+    // Read user name input
+    // get video Name
+    std::io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    println!("Enter Video URL:");
+    let mut get_url = String::new();
+    // Read video URL input
+    // get video URL
+    std::io::stdin()
+        .read_line(&mut get_url)
+        .expect("Failed to read line");
+
+    // Trim input URL
+    let url = get_url.trim();
+
+    // Download the best quality video
+    match rustube::download_best_quality(&url).await {
+        Ok(video_path) => {
+            let video_file_name = format!("{}.mp4", name.trim());
+            std::fs::rename(video_path, &video_file_name).expect("Failed to rename video file");
+            println!("Downloaded video as {}", video_file_name);
+        }
+        Err(err) => eprintln!("Error downloading video: {:?}", err),
+    };
+}
